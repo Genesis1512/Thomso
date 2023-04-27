10c8ab3633aa8745dc678ae1cdfa3637f65bc67f
Genesis1512 <127181089+Genesis1512@users.noreply.github.com>
2023-04-27 22:53:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function myFunction() 
+{
+    const x = document.getElementById("hash");
+    x.classList.toggle("responsive");
+
+  
+}
+
+
+//     if (x.className === "header") 
+//     {
+//       x.className += " responsive";
+
+//     }
+//      else
+//       {
+//       x.className = "header";
+//       }
+//   }
+
+
+
+
+
